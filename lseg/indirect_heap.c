a8ca75e49b2ae5689c3ec7e3095f7a72c3895fc7
remi delmas <remi.delmas.3000@gmail.com>
2023-08-13 12:39:41 -0400

    add offset when rebasing pointers

--- lseg/indirect_heap.c
+++ lseg/indirect_heap.c
@@ -4,42 +4,38 @@
 #include <stdlib.h>
 
 /*
-    An alternate heap model where a layer of indirection is added between
-    pointers of the user program and the actual heap.
+  An alternate heap model where a layer of indirection is added between
+  pointers of the user program and the actual heap.
 
-    The pointers manipulated by the user program are all dummy pointers pointing
-   to dummy objects of size 0.
+  The pointers manipulated by the user program are all dummy pointers pointing
+  to dummy objects of size 0.
 
-    These dummy pointers can be copied, we can perform arithmetic on them, but
-   they cannot be dereferenced directly because of the size 0.
+  These dummy pointers can be copied, we can perform arithmetic on them, but
+  they cannot be dereferenced directly because of the size 0.
 
-    The dummy pointers are used as indices into the actual heap model.
+  The dummy pointers are used as indices into the heap model.
 
-    The heap model contains either _real objects_, or _opaque objects_ with
-   associated _assumptions_.
+  The heap model maps dummy pointers to either _real objects_ or _assumptions_.
 
-    An opaque object is also allocated with size 0 and its contents cannot be
-   accessed.
+  When a dummy pointer does not have a corresponding real object in the heap model,
+  we say it is opaque.
 
-    The only action that can be done with an opaque object is to query the
-    existence of an assumption about it.
+  The only action that can be done with an opaque pointer is to query the
+  existence of an assumption for it.
 
-    An assumption describes the properties an opaque object would have if
-    concretized into a real object.
+  An assumption about an opaque pointer describes the properties it would have
+  if it were real.
 
-    The concretisation consumes the assumption, creates real objects and opaque
-    objects, and creates more assumptions about these opaque objects.
+  We can turn an assumption into real objects by unwinding the assumption,
+  Unwinding creates new real objects, new constraints and new opaque pointers
+  and assumptions.
 
-    Note that opaque objects and dummy objects serve different purposes:
-    - dummy objects are indices into the heap model.
-    - opaque objects are indices into a set of assumptions stored within the
-   heap model.
+  This indirection layer between the user program and the
+  heap allows modelling unbounded heaps using inductively
+  defined assumptions, unwinding assumptions dynamically
+  and checking inductively defined assertions as well.
 
-    The goal of having this indirection layer between the user program and the
-   heap is to allow changing the representation of the heap by adding new
-   assumptions, unfolding assumptions, etc. and have these changes being
-   transparent from the perspective of the user program.
-
+  This enables unbounded proofs through inductive reasoning. 
 */
 
 extern __CPROVER_size_t __CPROVER_max_malloc_size;
@@ -86,24 +82,24 @@ void __live_init() {
   __CPROVER_array_set(__live, false);
 }
 
-// map from dummy object IDs to real object IDs.
-void **__real_object;
+// map from dummy object IDs to base pointer of real object.
+void **__real_base;
 
-void __real_object_init() {
-  __real_object = __CPROVER_allocate(
-      __CPROVER_max_symex_objects * sizeof(*__real_object), 1);
-  __CPROVER_array_set(__real_object, false);
+void __real_base_init() {
+  __real_base = __CPROVER_allocate(
+      __CPROVER_max_symex_objects * sizeof(*__real_base), 1);
+  __CPROVER_array_set(__real_base, false);
 }
 
-// summons a fresh dummy pointer to make it ready to receive an assumption or a
+// __summons a fresh dummy pointer to make it ready to receive an assumption or a
 // real object
-void *summon() { return malloc(0); }
+void *__summon() { return malloc(0); }
 
 void *__malloc(size_t size) {
-  void *dummy = summon();
+  void *dummy = __summon();
   void *real = malloc(size);
   size_t id = __CPROVER_POINTER_OBJECT(dummy);
-  __real_object[id] = real;
+  __real_base[id] = real;
   __opaque[id] = false;
   __live[id] = true;
   return dummy;
@@ -116,31 +112,31 @@ void __free(void *dummy) {
   if (__opaque[id]) {
     __lseg[id] = (__lseg_t){.x = NULL, .y = NULL};
   } else {
-    free(__real_object[id]);
-    __real_object[id] = NULL;
+    free(__real_base[id]);
+    __real_base[id] = NULL;
   }
   __opaque[id] = true;
   __live[id] = false;
 }
 
-// fetches the real pointer for this dummy pointer (the object must be real)
-void *fetch_real(void *dummy) {
+// translates a dummy pointer into a pointer to real object
+void *__translate(void *dummy) {
   __CPROVER_precondition(!__opaque[__CPROVER_POINTER_OBJECT(dummy)],
                          "object is real");
-  return __real_object[__CPROVER_POINTER_OBJECT(dummy)];
+  return __real_base[__CPROVER_POINTER_OBJECT(dummy)] + __CPROVER_POINTER_OFFSET(dummy);
 }
 
 // posts an lseg(x, y) assumption
 void __assume_lseg(list_t *dummy_x, list_t *dummy_y) {
   size_t id = __CPROVER_POINTER_OBJECT(dummy_x);
-  __real_object[id] = NULL;
+  __real_base[id] = NULL;
   __opaque[id] = true;
   __live[id] = true;
   __lseg[id] = (__lseg_t){.x = dummy_x, .y = dummy_y};
 }
 
 // checks if an lseg(dummy_x, dummy_y) assumption exists and unfolds it
-void __unfold_lseg(void *dummy_x, void *dummy_y) {
+void __unwind_lseg(void *dummy_x, void *dummy_y) {
   __CPROVER_precondition(__opaque[__CPROVER_POINTER_OBJECT(dummy_x)],
                          "object is opaque");
   __CPROVER_precondition(
@@ -150,7 +146,7 @@ void __unfold_lseg(void *dummy_x, void *dummy_y) {
   size_t id = __CPROVER_POINTER_OBJECT(dummy_x);
   // create a real object for dummy_x
   list_t *real = malloc(sizeof(list_t));
-  __real_object[id] = real;
+  __real_base[id] = real;
   __opaque[id] = false;
   __lseg[id] = (__lseg_t){.x = NULL, .y = NULL};
 
@@ -159,83 +155,95 @@ void __unfold_lseg(void *dummy_x, void *dummy_y) {
 
   // the inductive case
   if (nondet_int()) {
-    real->next = summon();
+    real->next = __summon();
     __assume_lseg(real->next, dummy_y);
   }
 }
 
-// recursively evaluates lseg(x, y)
-bool eval_lseg(list_t *dummy_x, list_t *dummy_y, size_t lh) {
+// recursively evaluates lseg(x, y) ignoring separation constraints for now
+bool __eval_lseg(list_t *dummy_x, list_t *dummy_y, size_t lh) {
   __CPROVER_precondition(lh > 0, "max recursion depth");
   if (lh == 0)
+  {
+    // max depth reached, fail
     return false;
-  // ignoring separation constraints for now
+  }
   if (__opaque[__CPROVER_POINTER_OBJECT(dummy_x)]) {
     __lseg_t a = __lseg[__CPROVER_POINTER_OBJECT(dummy_x)];
+    // the assumption is for a different pointer, fail
     if (a.x != dummy_x)
       return false;
-    // success if we have an assumption lseg(dummy_x, dummy_y)
+    // found assumption lseg(dummy_x, dummy_y), succeed
     if (a.y == dummy_y)
       return true;
-    // we have an assumption for another dummy tail pointer
+    // found assumption for another tail pointer
     // try evaluating lseg(dummy_x, dummy_tail)
-    return eval_lseg(a.y, dummy_y, lh - 1);
+    return __eval_lseg(a.y, dummy_y, lh - 1);
   } else {
-    list_t *real_x = (list_t *)fetch_real(dummy_x);
+    // found a real object, evaluate recursively
+    list_t *real_x = (list_t *)__translate(dummy_x);
     if (real_x->next == dummy_y)
       return true;
-    return eval_lseg(real_x->next, dummy_y, lh - 1);
+    return __eval_lseg(real_x->next, dummy_y, lh - 1);
   }
 }
 
-// transformed program
+// __translate program
 void list_append(list_t *x, list_t *y) {
-  __unfold_lseg(x, NULL);
+  __unwind_lseg(x, NULL);
 
-  if (((list_t *)fetch_real(x))->next == NULL) {
-    ((list_t *)fetch_real(x))->next = y;
-    // check post conditions
-    assert(eval_lseg(y, NULL, 1));
-    assert(eval_lseg(x, y, 1));
+  if (((list_t *)__translate(x))->next == NULL) {
+    ((list_t *)__translate(x))->next = y;
+    // check post condition on exit
+    assert(__eval_lseg(y, NULL, 1));
+    assert(__eval_lseg(x, y, 1));
     return;
   }
 
-  list_t *tail = ((list_t *)fetch_real(x))->next;
+  list_t *tail = ((list_t *)__translate(x))->next;
 
   // loop invariant base case
-  assert(eval_lseg(x, tail, 1));
-  assert(eval_lseg(tail, NULL, 1));
+  assert(__eval_lseg(x, tail, 1));
+  assert(__eval_lseg(tail, NULL, 1));
 
   // loopback update
   if (nondet_int()) {
-    __assume_lseg(tail, NULL);
+    // summon fresh tail pointer
+    // and assume loop invariant between x and tail
+    tail = __summon();
     __assume_lseg(x, tail);
-    assert(eval_lseg(x, tail, 1));
+    __assume_lseg(tail, NULL);
+    // sanity check the invariant holds
+    assert(__eval_lseg(x, tail, 1));
+    assert(__eval_lseg(tail, NULL, 1));
   }
 
-  // sanity check
-  assert(eval_lseg(x, tail, 1));
+  // sanity check the invariant holds coming both from base and step case
+  assert(__eval_lseg(x, tail, 1));
+  assert(__eval_lseg(tail, NULL, 1));
 
-  __unfold_lseg(tail, NULL);
+  // unwind tail assumption
+  __unwind_lseg(tail, NULL);
 
-  // sanity check
-  assert(eval_lseg(x, tail, 2));
+  // sanity check the invariant still holds after unwinding
+  assert(__eval_lseg(x, tail, 2));
+  assert(__eval_lseg(tail, NULL, 2));
 
-  if (((list_t *)fetch_real(tail))->next != NULL) {
-    tail = ((list_t *)fetch_real(tail))->next;
+  if (((list_t *)__translate(tail))->next != NULL) {
+    tail = ((list_t *)__translate(tail))->next;
 
     // loop invariant
-    assert(eval_lseg(x, tail, 2));
-    assert(eval_lseg(tail, NULL, 1));
+    assert(__eval_lseg(x, tail, 2));
+    assert(__eval_lseg(tail, NULL, 1));
     __CPROVER_assume(false);
   }
   #ifndef BUG1
-  ((list_t *)fetch_real(tail))->next = y;
+  ((list_t *)__translate(tail))->next = y;
   #endif
 
   #ifdef BUG2
-  __unfold_lseg(y, NULL);
-  ((list_t *)fetch_real(y))->next = tail;
+  __unwind_lseg(y, NULL);
+  ((list_t *)__translate(y))->next = tail;
   #endif
 
 }
@@ -244,21 +252,21 @@ int main() {
   // ghost state setup
   __opaque_init();
   __live_init();
-  __real_object_init();
+  __real_base_init();
   __lseg_init();
 
   // preconditions
-  list_t *x = summon();
+  list_t *x = __summon();
   __assume_lseg(x, NULL);
 
-  list_t *y = summon();
+  list_t *y = __summon();
   __assume_lseg(y, NULL);
 
   // call function
   list_append(x, y);
 
   // postconditions
-  assert(eval_lseg(x, y, 2));
-  assert(eval_lseg(y, NULL, 2));
+  assert(__eval_lseg(x, y, 2));
+  assert(__eval_lseg(y, NULL, 2));
   return 0;
 }
